6572054ce942b08ac9cc6dc9c1a2d7a5aec3f85c
QUY\QUÝ <quy.nguyencord@hcmut.edu.vn>
2025-10-08 10:46:09 +0700

    1.1

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,70 @@ DHT dht(DHTPIN, DHTTYPE);
 
 uint32_t lastSend = 0;
 
+constexpr uint32_t MAX_MESSAGE_SIZE = 1024U;
+constexpr uint32_t SERIAL_DEBUG_BAUD = 115200U;
+
+constexpr char BLINKING_INTERVAL_ATTR[] = "blinkingInterval";
+constexpr char LED_MODE_ATTR[] = "ledMode";
+constexpr char LED_STATE_ATTR[] = "ledState";
+
+volatile bool attributesChanged = false;
+volatile int ledMode = 0;
+volatile bool ledState = false;
+
+constexpr uint16_t BLINKING_INTERVAL_MS_MIN = 10U;
+constexpr uint16_t BLINKING_INTERVAL_MS_MAX = 60000U;
+volatile uint16_t blinkingInterval = 1000U;
+
+uint32_t previousStateChange;
+
+constexpr int16_t telemetrySendInterval = 10000U;
+uint32_t previousDataSend;
+
+constexpr std::array<const char *, 2U> SHARED_ATTRIBUTES_LIST = {
+  LED_STATE_ATTR,
+  BLINKING_INTERVAL_ATTR
+};
+
+
+
+
+RPC_Response setLedSwitchState(const RPC_Data &data) {
+    Serial.println("Received Switch state");
+    bool newState = data;
+    Serial.print("Switch state change: ");
+    Serial.println(newState);
+    digitalWrite(LED_PIN, newState);
+    attributesChanged = true;
+    return RPC_Response("setLedSwitchValue", newState);
+}
+
+const std::array<RPC_Callback, 1U> callbacks = {
+  RPC_Callback{ "setLedSwitchValue", setLedSwitchState }
+};
+
+void processSharedAttributes(const Shared_Attribute_Data &data) {
+  for (auto it = data.begin(); it != data.end(); ++it) {
+    if (strcmp(it->key().c_str(), BLINKING_INTERVAL_ATTR) == 0) {
+      const uint16_t new_interval = it->value().as<uint16_t>();
+      if (new_interval >= BLINKING_INTERVAL_MS_MIN && new_interval <= BLINKING_INTERVAL_MS_MAX) {
+        blinkingInterval = new_interval;
+        Serial.print("Blinking interval is set to: ");
+        Serial.println(new_interval);
+      }
+    } else if (strcmp(it->key().c_str(), LED_STATE_ATTR) == 0) {
+      ledState = it->value().as<bool>();
+      digitalWrite(LED_PIN, ledState);
+      Serial.print("LED state is set to: ");
+      Serial.println(ledState);
+    }
+  }
+  attributesChanged = true;
+}
+
+const Shared_Attribute_Callback attributes_callback(&processSharedAttributes, SHARED_ATTRIBUTES_LIST.cbegin(), SHARED_ATTRIBUTES_LIST.cend());
+const Attribute_Request_Callback attribute_shared_request_callback(&processSharedAttributes, SHARED_ATTRIBUTES_LIST.cbegin(), SHARED_ATTRIBUTES_LIST.cend());
+
 // ================== WIFI ==================
 void connectWiFi()
 {
@@ -60,24 +124,11 @@ bool ensureConnected()
   }
   return true;
 }
-bool atribute_Data = false;
-RPC_Response setLedSwitchState(const RPC_Data &data){
-  Serial.println("Received Switch state");
-  bool newstate = data;
-  Serial.print("Switch state change: ");
-  Serial.println(newstate);
-  atribute_Data = true;
-  return RPC_Response("setLedSwitchValue", newstate);
-}
-
-const std::array<RPC_Callback, 1U> callbacks = {
-  RPC_Callback{"setLedSwitchValue", setLedSwitchState}
-};
-
 // ================== SETUP ==================
 void setup()
 {
   Serial.begin(SERIAL_BAUD);
+  pinMode(LED_PIN, OUTPUT);
   delay(1000);
 
   Serial.println("Khởi động...");
@@ -90,10 +141,10 @@ void loop()
 {
   if (!ensureConnected())
     return;
-  if (atribute_Data){
-    atribute_Data = false;
+  if (attributesChanged) {
+    attributesChanged = false;
+    tb.sendAttributeData(LED_STATE_ATTR, digitalRead(LED_PIN));
   }
-  else atribute_Data = true;
   if (millis() - lastSend >= SEND_INTERVAL)
   {
     lastSend = millis();
